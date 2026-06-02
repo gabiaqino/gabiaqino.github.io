761d79515529dd90e29a7ea1e1e6bf7e41db8320
gabiaqino <gabrielamelisa93@gmail.com>
2026-06-02 13:49:32 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,19 @@
-self.addEventListener('fetch', (event) => {
-  // Logic-caché
+const CACHE_NAME = "gabipos-v1";
+const URLS_TO_CACHE = ["/index.html", "/manifest.json"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
 });
